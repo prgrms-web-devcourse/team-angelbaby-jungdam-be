59aa9a667fc0c1388a785d36606470adc1ada20f
황일용 <1yongs_@naver.com>
2021-12-11 14:00:57 +0900

    🐛 [Fix/image-54] test code 오류 수정 (#55)

--- src/test/java/com/jungdam/JungdamApplicationTests.java
+++ src/test/java/com/jungdam/JungdamApplicationTests.java
@@ -1,13 +1,8 @@
 package com.jungdam;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class JungdamApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
 
 }

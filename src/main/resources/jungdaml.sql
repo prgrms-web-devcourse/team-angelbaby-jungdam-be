b55e00b23aeb4739aecb36a9e299225dc9dc77dd
황일용 <1yongs_@naver.com>
2021-12-13 15:41:30 +0900

    🚑 [Fix/cicd-61] mysql url 변경 및 jungdam.sql 스크립트 수정 (#62)

--- src/main/resources/jungdaml.sql
+++ src/main/resources/jungdaml.sql
@@ -1,5 +1,5 @@
 # CREATE DATABASE
-create database jungdam character set utf8mb4 default collate utf8_unicode_ci;
+create database jungdam character set utf8mb4 default collate utf8mb4_unicode_ci;
 
 # USE DATABASE
 use jungdam;

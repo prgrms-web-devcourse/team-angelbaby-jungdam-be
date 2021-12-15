771cb1b26b4bc60d728526492afcbc45e638664c
DongGeon0908 <50691225+DongGeon0908@users.noreply.github.com>
2021-12-16 03:59:18 +0900

    🐛 [Fix/diary-98] 미래의 날짜에 대한 일기 생성 금지 (#99)

--- src/main/java/com/jungdam/diary/domain/vo/RecordedAt.java
+++ src/main/java/com/jungdam/diary/domain/vo/RecordedAt.java
@@ -1,5 +1,7 @@
 package com.jungdam.diary.domain.vo;
 
+import com.jungdam.error.ErrorMessage;
+import com.jungdam.error.exception.InvalidArgumentException;
 import java.time.LocalDate;
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
@@ -14,7 +16,14 @@ public class RecordedAt {
     }
 
     public RecordedAt(LocalDate recordedAt) {
+        validate(recordedAt);
         this.recordedAt = recordedAt;
+    }
+
+    private void validate(LocalDate recordedAt) {
+        if (recordedAt.isAfter(LocalDate.now())) {
+            throw new InvalidArgumentException(ErrorMessage.INVALID_DIARY_RECORDED_AT);
+        }
     }
 
     public LocalDate getRecordedAt() {

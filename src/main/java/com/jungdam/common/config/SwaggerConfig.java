97aa0f820ee0b920e2cd61f9e76d20aaba43fcbc
황일용 <1yongs_@naver.com>
2021-12-18 00:30:41 +0900

    ✨ [Feature/swagger-135] Swagger 인증 처리 (#145)

--- src/main/java/com/jungdam/common/config/SwaggerConfig.java
+++ src/main/java/com/jungdam/common/config/SwaggerConfig.java
@@ -3,12 +3,17 @@ package com.jungdam.common.config;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -49,11 +54,29 @@ public class SwaggerConfig {
         Arrays.asList(CONTEXT_TYPE_JSON, CONTEXT_TYPE_XML)
     );
 
+    private ApiKey apiKey() {
+        return new ApiKey("JWT", "Authorization", "header");
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder().securityReferences(defaultAuth()).build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global",
+            "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return List.of(new SecurityReference("JWT", authorizationScopes));
+    }
+
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
             .apiInfo(DEFAULT_API_INFO)
             .produces(DEFAULT_PRODUCES_AND_CONSUMES)
-            .consumes(DEFAULT_PRODUCES_AND_CONSUMES);
+            .consumes(DEFAULT_PRODUCES_AND_CONSUMES)
+            .securityContexts(List.of(securityContext()))
+            .securitySchemes(List.of(apiKey()));
     }
 }

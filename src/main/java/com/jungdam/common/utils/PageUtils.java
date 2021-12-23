2b31338cd11bb10ed2c3373bbbdc9411302a8b31
soo5717 <koo5717@naver.com>
2021-12-23 13:35:26 +0900

    :recycle: PageUtils에 Sort 포함한 of 메서드 추가

--- src/main/java/com/jungdam/common/utils/PageUtils.java
+++ src/main/java/com/jungdam/common/utils/PageUtils.java
@@ -3,6 +3,7 @@ package com.jungdam.common.utils;
 import java.util.Objects;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
 public class PageUtils {
 
@@ -17,5 +18,12 @@ public class PageUtils {
             return PageRequest.of(DEFAULT_PAGE, DEFAULT_PAGE_SIZE);
         }
         return PageRequest.of(DEFAULT_PAGE, size);
+    }
+
+    public static Pageable of(Integer size, Sort sort) {
+        if (Objects.isNull(size)) {
+            return PageRequest.of(DEFAULT_PAGE, DEFAULT_PAGE_SIZE, sort);
+        }
+        return PageRequest.of(DEFAULT_PAGE, size, sort);
     }
 }
